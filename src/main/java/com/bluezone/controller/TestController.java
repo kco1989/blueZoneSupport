2f0e2887ff40516fd4564b34fec7238a32f22d1d
kco1989 <kco1989@qq.com>
2017-09-24 23:17:40 +0800

    Initial commit

---
+++ src/main/java/com/bluezone/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.bluezone.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+}
